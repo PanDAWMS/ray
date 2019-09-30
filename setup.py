729bf8bb1a9b07a662283ef1321b073d8c67a911
Julien Esseiva <esseivaju@gmail.com>
2019-09-30 14:05:21 -0700

    flake8

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-   name='Raythena',
-   version='1.0',
-   author='Miha Muskinja',
-   author_email='MihaMuskinja@lbl.gov',
-   packages=find_packages(),
-   scripts=[
-       'bin/raythena-event-service',
-       'bin/start-ray',
-   ],
-   install_requires=[
-       'ray',
-   ],
+    name='Raythena',
+    version='1.0',
+    author='Miha Muskinja',
+    author_email='MihaMuskinja@lbl.gov',
+    packages=find_packages(),
+    scripts=[
+        'bin/raythena-event-service',
+        'bin/start-ray',
+    ],
+    install_requires=[
+        'ray',
+    ],
 )
