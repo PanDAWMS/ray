f7f37aa8a96ea50c02676aa5e0244b923e34d83b
Julien Esseiva <esseivaju@gmail.com>
2019-12-06 11:55:13 -0800

    added custom exceptions

--- Raythena/utils/exception.py
+++ Raythena/utils/exception.py
@@ -2,13 +2,45 @@
 
 class BaseRaythenaException(Exception):
 
-    def __init__(self, actor_id, error_code=None, *args, **kwargs):
+    def __init__(self, id, error_code=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.actor_id = actor_id
+        self.id = id
         self.error_code = error_code
 
 
+class PluginNotFound(BaseRaythenaException):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class IllegalWorkerState(BaseRaythenaException):
+
+    def __init__(self, src_state, dst_state, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.src_state = src_state
+        self.dst_state = dst_state
+
+
+class StageInFailed(BaseRaythenaException):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class StageOutFailed(BaseRaythenaException):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
 class FailedPayload(BaseRaythenaException):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class UnknownException(BaseRaythenaException):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
