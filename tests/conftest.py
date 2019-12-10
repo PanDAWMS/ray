11ec68427cb289563759a6aa7b8be69556c28d3e
Julien Esseiva <esseivaju@gmail.com>
2019-12-10 14:51:58 -0800

    PluginsFactory tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,14 @@ from Raythena.utils.config import Config
 
 
 @pytest.fixture
-def config():
+def config_path():
+    return "tests/testconf.yaml"
+
+
+@pytest.fixture
+def config(config_path):
     print(f"cswd {os.getcwd()}")
     return Config(
-        "tests/testconf.yaml", config=None, debug=False, payload_bindir=None,
+        config_path, config=None, debug=False, payload_bindir=None,
         ray_driver=None, ray_head_ip=None, ray_redis_password=None, ray_redis_port=None,
         ray_workdir=None, harvester_endpoint=None, panda_queue=None, core_per_node=None)
---
+++ tests/test_plugins.py
@@ -0,0 +1,62 @@
+import pytest
+from Raythena.utils.plugins import PluginsRegistry
+
+
+class TestPlugins:
+
+    def test_package_namespace(self):
+        import Raythena.actors.payloads as payloads
+        import Raythena.actors.payloads.eventservice as payloads_es
+
+        plugins = PluginsRegistry()
+
+        # Checks that plugin module was registered after init
+        assert payloads.basePayload.__name__ in plugins.plugins
+        assert payloads_es.pilot2http.__name__ in plugins.plugins
+        assert payloads.__name__ not in plugins.plugins
+        assert payloads_es.__name__ not in plugins.plugins
+
+    def test_add_namespace(self):
+        import Raythena.drivers as drivers
+        plugins = PluginsRegistry()
+        plugins.add_plugin_namespace(drivers)
+        assert drivers.__name__ not in plugins.plugins
+        assert drivers.esdriver.__name__ in plugins.plugins
+
+    def test_get_plugins(self, config):
+        import Raythena.actors.payloads.eventservice as payloads_es
+        plugins = PluginsRegistry()
+
+        with pytest.raises(ValueError):
+            assert plugins.get_plugin("unknown")
+
+        plugin_str = config.payload['plugin']
+        assert plugin_str
+        module, sep, plugin_class = plugin_str.partition(":")
+        print(f"Module: {module}, class: {plugin_class}")
+        with pytest.raises(ValueError):
+            plugins.get_plugin(module)
+
+        with pytest.raises(ValueError):
+            plugins.get_plugin(plugin_class)
+
+        with pytest.raises(ImportError):
+            plugins.get_plugin(f"{plugin_str}_donotexists")
+
+        with pytest.raises(ImportError):
+            plugins.get_plugin(f"donotexists.{plugin_str}")
+
+        with pytest.raises(ImportError):
+            plugins.get_plugin(f"{module}.donotexists:{plugin_class}")
+
+        plugin_str_full = f"{payloads_es.__name__}.{plugin_str}"
+        pilot_http_plugin = plugins.get_plugin(plugin_str_full)
+
+        assert pilot_http_plugin is not None
+        assert issubclass(pilot_http_plugin, payloads_es.pilot2http.Pilot2HttpPayload)
+        assert issubclass(pilot_http_plugin, payloads_es.esPayload.ESPayload)
+
+        pilot_http_plugin = plugins.get_plugin(plugin_str)
+        assert pilot_http_plugin is not None
+        assert issubclass(pilot_http_plugin, payloads_es.pilot2http.Pilot2HttpPayload)
+        assert issubclass(pilot_http_plugin, payloads_es.esPayload.ESPayload)
