d518ffd3cb62a2cd602795cc4082b7dbd9f1ea3a
Julien Esseiva <esseivaju@gmail.com>
2020-01-16 16:14:04 -0800

    CPUMonitor doc

--- raythena/utils/timing.py
+++ raythena/utils/timing.py
@@ -3,13 +3,16 @@ import time
 import json
 
 from threading import Event
+from typing import Any, Dict, List, Union
 
 from raythena.utils.exception import ExThread
 
 
 class CPUMonitor:
-
-    def __init__(self, log_file, pid=None):
+    """
+    Monitoring tools recording system cpu utilization as well as process cpu utilization to a file
+    """
+    def __init__(self, log_file: str, pid: Any = None) -> None:
         self.process = psutil.Process(pid)
         self.log_file = log_file
         self.stop_event = Event()
@@ -17,22 +20,63 @@ class CPUMonitor:
         self.write_interval = 1 * 60
         self.time_step = 1
 
-    def start(self):
+    def start(self) -> None:
+        """
+        Start the cpu monitoring thread
+
+        Returns:
+            None
+        """
         if not self.monitor_thread.is_alive():
             self.monitor_thread.start()
 
-    def stop(self):
+    def stop(self) -> None:
+        """
+        Stop the monitoring thread and reset state to allow to restart monitoring
+
+        Returns:
+            None
+        """
         if not self.stop_event.is_set():
             self.stop_event.set()
             self.monitor_thread.join()
             self.monitor_thread = ExThread(target=self.monitor_cpu, name="cpu_monitor")
             self.stop_event = Event()
 
-    def _log_to_file(self, data):
+    def _log_to_file(self, data: Dict[str, Union[Dict[str, List], List, int]]) -> None:
+        """
+        Write data to log file
+
+        Args:
+            data: dict holding cpu time and utilization
+
+        Returns:
+            None
+        """
         with open(self.log_file, 'w') as f:
             json.dump(data, f)
 
-    def monitor_cpu(self):
+    def monitor_cpu(self) -> None:
+        """
+        Records cpu utilization, cpu times for the monitored process and system-wide cpu utilization every second.
+        Cpu utilization data are saved to a dict using the following structure:
+
+        {
+            "system_usage": [...],
+            "process_usage": [...],
+            "process_times": {
+                "user": [..],
+                "system": [..],
+                "iowait": [..] # Linux only
+                },
+            "time_step": 1
+        }
+
+        The dict is dumped to the logfile every minute and when the thread is stopped
+
+        Returns:
+            None
+        """
         last_write = 0
         _ = psutil.cpu_percent()
         _ = self.process.cpu_percent()
