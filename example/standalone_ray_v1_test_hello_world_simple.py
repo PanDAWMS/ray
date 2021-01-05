541636669cf49143af1a71d3580fa2c26606d4e5
Doug Benjamin <dbenjamin@anl.gov>
2021-01-05 14:35:51 +0000

    flake8 fixes

--- example/standalone_ray_v1_test_hello_world_simple.py
+++ example/standalone_ray_v1_test_hello_world_simple.py
@@ -13,22 +13,24 @@ from pprint import pprint
 import ray
 import time
 
-def build_nodes_resource_list(redis_ip):
+
+def build_nodes_resource_list(redis_ip: str):
     nodes = ray.nodes()
     resource_list = list()
     for node in nodes:
         naddr = node['NodeManagerAddress']
         if naddr == redis_ip:
-            continue 
+            continue
         else:
             resource_list.append(node)
     return resource_list
 
+
 @ray.remote
 class actor():
-    def __init__(self):
+    def __init__(self) -> None:
         self.pid = os.getpid()
-        self.hostname = platform.node() 
+        self.hostname = platform.node()
         self.ip = ray._private.services.get_node_ip_address()
         print(f"Initial message from PID - {self.pid} Running on host - {self.hostname} {self.ip}")
 
@@ -38,17 +40,16 @@ class actor():
         return f"{self.pid} {self.hostname} {self.ip} - pong"
 
 
-def main(args):
-    print(f"args : {args}")
-    redis_address = f"{args.redis_ip}:{args.redis_port}"
+def main(redis_ip: str, redis_port: str, redis_password: str):
+    redis_address = f"{redis_ip}:{redis_port}"
     ray.init(ignore_reinit_error=True,
-             address="%s" % redis_address, _redis_password="%s" % args.redis_password)
+             address="%s" % redis_address, _redis_password="%s" % redis_password)
 
     # show the ray cluster
     print(f"Ray Cluster resources : {ray.cluster_resources()}")
-  
+
     # Get list of nodes to use
-    nodes = build_nodes_resource_list(args.redis_ip)
+    nodes = build_nodes_resource_list(redis_ip)
     print(f"Found {len(nodes)} Worker nodes in the Ray Cluster:")
     pprint(nodes)
     print(" ")
@@ -71,11 +72,11 @@ def main(args):
     [ray.kill(actor) for actor in actors]
 
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Wait on ray head node or workers to connect')
     parser.add_argument('--redis-ip', default="%s" % (os.environ["RAYTHENA_RAY_HEAD_IP"]))
     parser.add_argument('--redis-port', default="%s" % (os.environ["RAYTHENA_RAY_REDIS_PORT"]))
     parser.add_argument('--redis-password', default=os.environ["RAYTHENA_RAY_REDIS_PASSWORD"])
     args = parser.parse_args()
-    main(args)
+    print(f"args : {args}")
+    main(args.redis_ip, args.redis_port, args.redis_password)
