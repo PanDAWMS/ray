bd50dbfdd11a783742a2baa27a0970db1c5d9694
Doug Benjamin <dbenjamin@anl.gov>
2021-01-05 14:40:21 +0000

    make changes for ray V1

--- example/standalone_ray_test_hello_world.py
+++ example/standalone_ray_test_hello_world.py
@@ -1,60 +1,82 @@
 #!/usr/bin/env python
 """
 A simple test of Ray
+
+This example uses placement_group API to spread work around
+
 """
 
 import argparse
 import os
+import platform
+from pprint import pprint
 import ray
 import time
 
 
-def build_nodes_resource_list(args):
+def build_nodes_resource_list(redis_ip: str):
     nodes = ray.nodes()
     resource_list = list()
-    custom_resource = "payload_slot"
     for node in nodes:
         naddr = node['NodeManagerAddress']
-        node_custom_resource = f"{custom_resource}@{naddr}"
-        ray.experimental.set_resource(node_custom_resource, 1, node['NodeID'])
-        resource_list.extend([node_custom_resource])
+        if naddr == redis_ip:
+            continue
+        else:
+            resource_list.append(node)
     return resource_list
 
 
 @ray.remote
 class actor():
-    def __init__(self, name):
-        self.name = name
-        print(f"Initial message from {self.name}")
+    def __init__(self) -> None:
+        self.pid = os.getpid()
+        self.hostname = platform.node()
+        self.ip = ray._private.services.get_node_ip_address()
+        print(f"Initial message from PID - {self.pid} Running on host - {self.hostname} {self.ip}")
 
     def ping(self):
-        print("ping")
+        print(f"{self.pid} {self.hostname} {self.ip} - ping")
         time.sleep(5)
-        return "pong"
+        return f"{self.pid} {self.hostname} {self.ip} - pong"
 
 
-def main(args):
+def main(redis_ip: str, redis_port: str, redis_password: str):
+    redis_address = f"{redis_ip}:{redis_port}"
     ray.init(ignore_reinit_error=True,
-             address="%s" % args.redis_address, redis_password="%s" % args.redis_password)
+             address="%s" % redis_address, _redis_password="%s" % redis_password)
 
-    # Setup one Actor per ndoe
-    nodes = build_nodes_resource_list(args)
-    print(f"Found {len(nodes)} nodes in the Ray Cluster:\n\t{nodes}")
+    # show the ray cluster
+    print(f"Ray Cluster resources : {ray.cluster_resources()}")
+
+    # Get list of nodes to use
+    nodes = build_nodes_resource_list(redis_ip)
+    print(f"Found {len(nodes)} Worker nodes in the Ray Cluster:")
+    pprint(nodes)
+    print(" ")
+
+    # Setup one Actor per node
     print(f"Setting up {len(nodes)} Actors...")
-    actors = [actor.options(resources={node: 1}).remote(f"Actor_{node.split('@')[1]}") for node in nodes]
+    actors = []
+    for node in nodes:
+        node_ip_str = f"node:{node['NodeManagerAddress']}"
+        actors.append(actor.options(resources={f"{node_ip_str}": 1}).remote())
     time.sleep(1)
 
     # Ping-Pong test
-    for _ in range(10):
+    for _ in range(5):
         messages = [actor.ping.remote() for actor in actors]
         for msg in ray.get(messages):
             print(f"Received back message {msg}")
 
+    # now terminate the actors
+    [ray.kill(actor) for actor in actors]
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Wait on ray head node or workers to connect')
-    parser.add_argument('--redis-address', default="%s:%s" % (os.environ["RAYTHENA_RAY_HEAD_IP"], os.environ["RAYTHENA_RAY_REDIS_PORT"]))
+    parser.add_argument('--redis-ip', default="%s" % (os.environ["RAYTHENA_RAY_HEAD_IP"]))
+    parser.add_argument('--redis-port', default="%s" % (os.environ["RAYTHENA_RAY_REDIS_PORT"]))
     parser.add_argument('--redis-password', default=os.environ["RAYTHENA_RAY_REDIS_PASSWORD"])
-    parser.add_argument('--worker-per-node', default=os.environ["RAYTHENA_RAY_NWORKERS"])
     args = parser.parse_args()
-    main(args)
+    print(f"args : {args}")
+    main(args.redis_ip, args.redis_port, args.redis_password)
