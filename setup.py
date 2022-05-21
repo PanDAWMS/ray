05470f73fa54d531eaa0f1f4f577affc87a8e5e6
Julien Esseiva <esseivaju@gmail.com>
2022-05-20 20:06:44 -0700

    moved raythena start command as entrypoint

--- setup.py
+++ setup.py
@@ -1,20 +1,33 @@
 from setuptools import setup, find_packages
-from raythena import __version__
+import re
+
+
+def find_version(path):
+    with open(path) as fp:
+        version_match = re.search(
+            r"^__version__ = ['\"]([^'\"]*)['\"]", fp.read(), re.M
+        )
+        if version_match:
+            return version_match.group(1)
+
 
 setup(
     name='raythena',
-    version=__version__,
+    version=find_version('src/raythena/__init__.py'),
     author='Miha Muskinja',
     author_email='MihaMuskinja@lbl.gov',
-    packages=find_packages(where="src"),
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     scripts=[
         'bin/ray_start_head',
         'bin/ray_start_worker',
         'bin/ray_sync',
-        'bin/raythena.py',
         'example/setup_ray_cluster_slurm.sh',
         'example/standalone_ray_test_hello_world.py',
     ],
+    entry_points={
+        'console_scripts': ['raythena=raythena.scripts.raythena:main']
+    },
     data_files=[
         ('conf', ['conf/cori.yaml'])
     ],
